8554aaceb25de4ba2ef60733df49492229a32b77
Bhavik <interns@jcasptechnologies.com>
2026-08-10 12:53:37 +0530

    chore: understood variables and how to intialize them with var, let, const

---
+++ learning_js/es6/Variables.js
@@ -0,0 +1,8 @@
+// let vs var vs const
+// const → value/reference won't be reassigned
+// let   → value needs reassignment
+// var   → generally avoid
+
+var firstName = "Bhavik"; // Global Variable
+let lastName = "Sharma"; // Block Scoped Variable (can be modified)
+const bloodGroup = "AB+"; // Block Scoped Variable (cannot be modified)
