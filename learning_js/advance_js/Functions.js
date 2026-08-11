b14287b4abe364e3d93dafc4dab4a02ddba1539b
Bhavik <interns@jcasptechnologies.com>
2026-08-11 17:14:23 +0530

    Practiced Functions in JS

---
+++ learning_js/advance_js/Functions.js
@@ -0,0 +1,22 @@
+// Function Declaration: name is parameter
+// function getMyName(name) {
+//   console.log(name);
+// }
+
+// Function Calling:
+// getMyName("Bhavik");
+// // "Bhavik" is Argument
+
+// Function Expression: Used Arrow Function
+// const getMyAge = (age) => {
+//   console.log(age);
+// };
+// getMyAge(21);
+
+// Scope: Function has its Local scope where only its variables are accessible.
+// function getMyNumber() {
+//   let number = "12345567890";
+//   console.log(number); // accessible
+// }
+// getMyNumber();
+// console.log(number); // not accessible bcoz number variable is created inside function local scope until it is returned.
